0b2016115292f40ea6f32bdb122601e9af0e8ee0
clinteads <clint.eads@gmail.com>
2026-08-19 21:11:44 -0600

    Catch

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	pokeClient := pokeapi.NewClient(5 * time.Second, time.Minute*5)
 	cfg := &config{
 		commands:      getCommands(),
+		caughtPokemon: map[string]pokeapi.Pokemon{},
 		pokeapiClient: pokeClient,
 	}
 
